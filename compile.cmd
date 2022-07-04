c0f7516d18533bed7f8e7904f316f893cbfaa3e8
teodias <teodias@gmx.net>
2022-07-04 14:09:05 +0200

    compile.cmd updated

--- compile.cmd
+++ compile.cmd
@@ -1,5 +1,3 @@
 @ECHO OFF
-
-  pyinstaller --noconfirm --log-level=WARN --clean --onefile --nowindow --add-data="wordlist.txt;." --name ke-password ke-password.pyw
-
+  scripts\pyinstaller --noconfirm --log-level=WARN --clean --onefile --nowindow --name ke-password ke-password.pyw
 GOTO :EOF
